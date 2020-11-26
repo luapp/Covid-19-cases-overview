eb684b209b7910e5b67964467756de4ec65b44b2
Creator-360 <42955270+Creator-360@users.noreply.github.com>
2020-11-26 16:42:18 +0800

    Update & fix

--- public/electron.js
+++ public/electron.js
@@ -1,38 +1,10 @@
 const { app, BrowserWindow, Menu } = require('electron')
-//const { autoUpdater } = require("electron-updater")
-
-const Bug_Report_window = () => {
-  const window = new BrowserWindow({
-    width: 640,
-    height: 500,
-    center: true,
-    maximizable: false,
-    minimizable: false,
-    webPreferences: {
-      nodeIntegration: false
-    }
-  })
-
-  window.loadFile("../electron/Bug_Report.html")
-}
 
 function Main_window () {
 
   const Menu_Bar = Menu.buildFromTemplate([
     {
-      label: "About",
-      click(){
-        Bug_Report_window()
-      }
-    },
-    {
-      role: "separator"
-    },
-    {
-      label: "Check for updates",
-      click(){
-        Bug_Report_window()
-      }
+      label: "By Paul Le Gall  (Github: https://github.com/Creator-360/Covid-19-cases-overview)",
     }
   ])
 
